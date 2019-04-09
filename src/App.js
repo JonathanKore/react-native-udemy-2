fc2881e9dcaaf258202555c995db2d153aed8a44
Jonathan Köre <korejonathan@gmail.com>
2019-04-09 17:05:30 +0200

    trying to add travis yml file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyDccAp3QEEt4xqSufBWQ5WAZZ1T7qVuBLU',
@@ -13,13 +15,41 @@ class App extends Component {
             projectId: 'authentication-596d8',
             storageBucket: 'authentication-596d8.appspot.com',
             messagingSenderId: '862486279853'
-          });
+        });
+        
+        //Denna övervakar typ? fett weird
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
     }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    //Custom för android, kanske inte ens är den smarrigaste lösningen
+                    <View style={{ flexDirection: 'row' }}>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </View>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large" />;
+        }
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
